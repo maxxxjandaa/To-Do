97ac626888f0a3b97c0451cb769c6a6c7110e52c
Max <maxmilian.janda@gmail.com>
2024-02-19 13:39:31 +0100

    Přidal jsem nějaký ten script

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const toDoBtn = document.getElementById("ToDo_btn");
-const mainContent = document.querySelector("main");
-
-toDoBtn.addEventListener("click", () => {});
+const showBtn = document.getElementById("add-btn");
+const myDisplay = document.getElementById("information");
+let look = "none";
+showBtn.addEventListener("click", () => {
+  if (look == "none") {
+    myDisplay.style.display = "flex";
+    look = "flex";
+  } else if ((look = "flex")) {
+    myDisplay.style.display = "none";
+    look = "none";
+  }
+});
